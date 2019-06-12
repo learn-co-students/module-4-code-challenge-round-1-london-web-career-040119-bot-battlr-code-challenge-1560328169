00227749eaed38758a76509654dc2bb380172220
hlbtyne <harriet.ballantyne@gmail.com>
2019-06-12 10:09:19 +0100

    Checkpoint reached

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,38 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
+
+const botsURL = 'https://bot-battler-api.herokuapp.com/api/v1/bots'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  
+  state = {
+    bots: [],
+    enlistedBots: []
+  }
+
+  componentDidMount = () => {
+    fetch(botsURL)
+    .then(resp => resp.json())
+    .then(bots => this.setState({ bots }))
+  }
+
+  enlistBot = bot => {
+    if (this.state.enlistedBots.includes(bot)) return
+
+    this.setState({ enlistedBots: [...this.state.enlistedBots, bot] })
+  }
+
+  releaseBot = releasedBot => {
+    const arrayMinusBot = [...this.state.enlistedBots].filter(bot => releasedBot !== bot)
+    this.setState({ enlistedBots: arrayMinusBot })
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+          < YourBotArmy yourBots={this.state.enlistedBots} release={this.releaseBot}/>
+          < BotCollection bots={this.state.bots} enlist={this.enlistBot}/>
       </div>
     );
   }
